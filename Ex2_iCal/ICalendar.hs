dbf5af73ec48b5f6f24968e48e9cfb85a86b08e7
Jesse <stolwijkjesse@gmail.com>
2016-12-06 19:21:26 +0100

    Opdracht 2 begonnen

--- Ex2_iCal/ICalendar.hs
+++ Ex2_iCal/ICalendar.hs
@@ -1,10 +1,9 @@
 module ICalendar where
-import Prelude hiding (sequence, (<*>), (<$>), (<$), parse, (<*))
---http://hackage.haskell.org/package/pretty-1.1.3.4/docs/Text-PrettyPrint-HughesPJ.html#v:render
-import Text.PrettyPrint.HughesPJ
+import Prelude hiding (sequence, (<*>), (<$>), (<$), parse, (<*), (*>))
+import Text.PrettyPrint
 import ParseLib.Abstract
 import Data.Maybe
-
+import Data.Char
 
 data DateTime = DateTime { date :: Date
                          , time :: Time
@@ -50,7 +49,6 @@ run p s = listToMaybe [p | (p, []) <- parse p s]
 recognizeCalendar :: String -> Maybe Calendar
 recognizeCalendar s = run scanCalendar s >>= run parseCalendar
 
-
 -- "Main" block, DO NOT EDIT.
 -- If you want to run the parser + pretty-printing, rename this module (first line) to "Main".
 -- DO NOT forget to rename the module back to "ICalendar" before submitting to DomJudge.
@@ -60,24 +58,52 @@ main = do
 
 
 -- Exercise 1
-data Token = Token
-    deriving (Eq, Ord, Show)
+data Token = Tstartcal 
+            | Tendcal
+            | Tstartevent
+            | Tendevent
+            | Tdtstamp DateTime
+            | Tdtstart DateTime
+            | Tdtend DateTime
+            | Tuid String
+            | Tsummary String
+    deriving (Eq, Ord)
+spaces :: Parser Char String
+spaces = greedy (satisfy isSpace)
+
+tstartcal :: Parser Char Token
+tstartcal = Tstartcal <$ token "BEGIN:VCALENDAR"
+
+tendcal :: Parser Char Token
+tendcal = Tendcal <$ token "END:VCALENDAR"
+
+tstartevent :: Parser Char Token
+tstartevent = Tstartevent <$ token "BEGIN:VEVENT"
+
+tendevent :: Parser Char Token
+tendevent = Tendevent <$ token "END:VEVENT"
+
+tstuid :: Parser Char Token
+tstuid = undefined --Tuid <$ token "UID:" -- <*> token
+
+tdtstamp :: Parser Char Token
+tdtstamp = Tdtstamp <$ token "DTSTAMP:" <*> parseDateTime
+
+tdtstart :: Parser Char Token
+tdtstart = Tdtstart <$ token "DTSTART:" <*> parseDateTime
+
+tdtend :: Parser Char Token
+tdtend = Tdtend <$ token "DTEND:" <*> parseDateTime
+
+anyToken :: Parser Char Token
+anyToken = tstartcal <|> tstartevent <|> tdtstamp <|> tdtstart <|> tdtend <|> tendevent <|> tendcal
 
 scanCalendar :: Parser Char [Token]
-scanCalendar = undefined
+scanCalendar = spaces *> greedy anyToken <* eof
 
 --parse date
 parseDate :: Parser Char Date
-parseDate = Date <$> parseYear <*> parseMonth <*> parseDay 
-
-parseYear :: Parser Char Year
-parseYear = Year <$> parseInt 4
-
-parseMonth :: Parser Char Month
-parseMonth = Month <$> parseInt 2
-
-parseDay :: Parser Char Day
-parseDay = Day <$> parseInt 2
+parseDate = Date <$> (Year <$> parseInt 4) <*> (Month <$> parseInt 2) <*> (Day <$> parseInt 2) 
 
 
 --Parse time
@@ -106,20 +132,39 @@ numberGen = foldl(\x y -> 10*x + y) 0
 parseInt :: Int -> Parser Char Int
 parseInt n = numberGen <$> sequence (replicate n newdigit)
 
+
 parseDateTime :: Parser Char DateTime
 parseDateTime = DateTime <$> parseDate <* parseTimeSeperator <*> parseTime <*> parseUTC 
 
---stampParse :: Parser Char DateTime
-stampParse x = sequence $ map symbol x
 parseUid :: Parser Char String
 parseUid = undefined
 
-startCal :: Parser Char String
-startCal = sequence (map symbol "ada" )
+{-
+datetime :: Token -> DateTime
+datetime t = fromDateTime <$> (satisfy isDateTime t)
 
+isDateTime :: Token -> Bool
+isDateTime (DateTime x) = True
+isDateTime _= False
+
+fromDateTime :: Token -> DateTime
+fromDateTime (DateTime x) = x
+fromDateTime _ = error "fromDateTime"-}
+{-
+checkString :: Token -> String
+checkString = fromString <$> satisfy IsString
+
+isString :: Token -> Bool
+isString t :: [Char] = True
+isString _ = False
+fromString :: Token -> String
+fromString t :: [Char] = t
+fromString _ = error "fromDateTime"-}
 parseCalendar :: Parser Token Calendar
-parseCalendar = undefined--Calendar <$> startCal parseDateTime <*> parseUid <*> parseDateTime <*> parseDateTime <*> parseUid <*> parseUid <*> parseUid
+parseCalendar = undefined--Calendar <$> datetime <*> checkString <*> datetime <*> datetime <*> checkString <*> checkString <*> checkString
 
+parseVEvent :: Parser Token VEvent
+parseVEvent = undefined--VEvent <$> 
 
 
 -- Exercise 2
@@ -127,28 +172,112 @@ readCalendar :: FilePath -> IO (Maybe Calendar)
 readCalendar = undefined
 
 
+dt = (DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 01) (Second 01)) True) 
+dt' = (DateTime (Date (Year 1997) (Month 07) (Day 17)) (Time (Hour 17) (Minute 01) (Second 02)) True) 
+dt2 = (DateTime (Date (Year 1997) (Month 07) (Day 15)) (Time (Hour 17) (Minute 01) (Second 02)) True) 
+
+vev = (VEvent dt "19970610T172345Z-AF23B2@example.com" dt dt' (Just "a") (Just "a") (Just "a" ))
+exCal = Calendar "-//hacksw/handcal//NONSGML v1.0//EN" [vev]
 -- Exercise 3
 -- DO NOT use a derived Show instance. Your printing style needs to be nicer than that :)
 printCalendar :: Calendar -> String
-printCalendar = undefined
+printCalendar (Calendar pid xs) = printBeginCal++ "\r\n" ++ "PRODID:" ++ pid ++ "\r\n"++ concatMap printVEvent xs ++  printEndCal 
 
+printVEvent :: VEvent -> String
+printVEvent (VEvent stamp uid s e d summ loc) = printBeginEvent ++ "\r\n" ++
+                                                "DTSTAMP:" ++ printDateTime stamp ++ "\r\n"++ 
+                                                "DTSTART:" ++ printDateTime s++ "\r\n" ++ 
+                                                "DTEND:" ++ printDateTime e ++ "\r\n" ++ 
+                                                "UID:" ++ uid ++ "\r\n"++ 
+                                                printMaybe d "DESCRIPTION:" ++ 
+                                                printMaybe summ "SUMMARY:" ++ 
+                                                printMaybe loc "LOCATION:" ++
+                                                printEndEvent ++ "\r\n"
+
+printBeginCal = "BEGIN:VCALENDAR"
+printEndCal = "END:VCALENDAR"
+printBeginEvent = "BEGIN:VEVENT"
+printEndEvent = "END:VEVENT"
+
+printMaybe (Just x) p = p ++ x ++ "\r\n"
+printMaybe Nothing _ = ""
+
+printDateTime :: DateTime -> String
+printDateTime (DateTime (Date (Year y) (Month m) (Day d)) (Time (Hour h) (Minute mi) (Second s)) utc) 
+    = f [y,m,d] ++ "T" ++ f [h,mi,s] ++ printUTC utc
+        where f = concatMap showData
+
+printUTC :: Bool -> String
+printUTC True = "Z"
+printUTC False = ""
+
+--add 0 if number is a single digit
+showData :: (Show a) => a -> String
+showData a = if null xs then "0" ++ [x] else o
+    where o@(x:xs) = show a
 
 -- Exercise 4
 countEvents :: Calendar -> Int
-countEvents = undefined
+countEvents (Calendar pid xs) = length xs
 
 findEvents :: DateTime -> Calendar -> [VEvent]
-findEvents = undefined
+findEvents dt (Calendar _ xs) = foldr (checkEvent dt) [] xs
+
+checkEvent :: DateTime -> VEvent -> [VEvent] -> [VEvent]
+checkEvent dt e xs = if start < dt && end > dt then e:xs else xs
+    where start = dtStart e
+          end = dtEnd e
+
 
 checkOverlapping :: Calendar -> Bool
 checkOverlapping = undefined
 
 timeSpent :: String -> Calendar -> Int
-timeSpent = undefined
+timeSpent s (Calendar _ xs) = foldr (\c x -> if fromJust'(summary c) == s then x + (minusDateTime (dtStart c) (dtEnd c)) else x) 0 xs
+    where fromJust' (Just x) = x
+          fromJust' _ = ""
+minusDateTime (DateTime (Date (Year y) (Month m) (Day d)) (Time (Hour h) (Minute mi) (Second s)) _) 
+              (DateTime (Date (Year y2) (Month m2) (Day d2)) (Time (Hour h2) (Minute mi2) (Second s2)) _) 
+              = (d2 - d) * 60 * 24 + (h2 - h) * 60 + mi2 - mi + quot (s2 - s) 60
 
 
+
+data EventWeek = EventWeek { eventdays :: [EventDay]}
+
+data EventDay = EventDay {  unDate :: Date,
+                            unEvents :: [VEvent] }
 
 -- Exercise 5
 ppMonth :: Year -> Month -> Calendar -> Doc
-ppMonth = undefined
+ppMonth =undefined
 
+createEventWeek :: Calendar -> Date -> EventWeek
+createEventWeek c d = EventWeek $ map (findEventsDay c) (createDateList d dayList)
+    where dayList = [1..(daysInMonth (month d) (year d))]
+
+findEventsDay :: Calendar -> Date -> EventDay
+findEventsDay (Calendar _ xs) d = EventDay (d) (foldr (\e as -> if date (dtStart e) == d then e : as else as) [] xs)
+
+createDateList :: Date -> [Int] -> [Date]
+createDateList (Date (Year y) (Month m) (Day d)) ds = foldr (\c xs -> ((Date (Year y) (Month m) (Day c)):xs)) [] ds
+
+
+--returns days in month (needs year for leap year check)                                       
+daysInMonth :: Month -> Year -> Int
+daysInMonth (Month m) y = ds !! (m - 1)
+    where ds = [31,febDays y,31,30,31,30,31,31,30,31,30,31]
+
+febDays:: Year -> Int
+febDays y 
+    | leapYear y = 29
+    | otherwise = 28
+
+leapYear :: Year -> Bool
+leapYear (Year y)
+    | modBool y 400 = True
+    | modBool y 100 = False
+    | modBool y 4 = True
+    | otherwise = False
+
+modBool :: Int -> Int -> Bool
+modBool x y = mod x y == 0
