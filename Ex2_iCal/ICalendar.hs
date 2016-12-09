218061200ed20475ff054ff280561b186321dfb3
Jesse <stolwijkjesse@gmail.com>
2016-12-09 16:17:41 +0100

    hi

--- Ex2_iCal/ICalendar.hs
+++ Ex2_iCal/ICalendar.hs
@@ -92,27 +92,43 @@ parseUTC :: Parser Char Bool
 parseUTC = True <$ symbol 'Z' <|> False <$ epsilon
 
 parseDateTime :: Parser Char DateTime
-parseDateTime = DateTime <$> parseDate <* (symbol 'T') <*> parseTime <*> parseUTC
+parseDateTime = DateTime <$> parseDate <* symbol 'T' <*> parseTime <*> parseUTC
 
 readStringTillEnd :: Parser Char String
 readStringTillEnd = many $ satisfy (\x -> x /= '\n' && x /= '\r')
 
+--Deze variabelen worden bij het lexen en het printen gebruikt
+printBeginCal = "BEGIN:VCALENDAR"
+printVersion = "VERSION:2.0"
+printProdID = "PRODID:"
+printBeginEvent = "BEGIN:VEVENT"
+printDtStamp = "DTSTAMP:"
+printDtStart = "DTSTART:"
+printDtEnd = "DTEND:"
+printUID = "UID:"
+printSummary = "SUMMARY:"
+printLocation = "LOCATION:" 
+printDescription = "DESCRIPTION:" 
+printEndEvent = "END:VEVENT"
+printEndCal = "END:VCALENDAR"
+printCrlf = "\r\n" 
+
 anyToken :: Parser Char Token
-anyToken = Tstartcal    <$ token "BEGIN:VCALENDAR"                          <* newLine <|>
-           Tempty       <$ token "VERSION:2.0"                              <* newLine <|>
-           Tprodid      <$ token "PRODID:"          <*> readStringTillEnd   <* newLine <|>
-           Tstartevent  <$ token "BEGIN:VEVENT"                             <* newLine <|> 
-           Tdtstamp     <$ token "DTSTAMP:"         <*> parseDateTime       <* newLine <|> 
-           Tdtstart     <$ token "DTSTART:"         <*> parseDateTime       <* newLine <|> 
-           Tdtend       <$ token "DTEND:"           <*> parseDateTime       <* newLine <|> 
-           Tuid         <$ token "UID:"             <*> readStringTillEnd   <* newLine <|>
-           Tsummary     <$ token "SUMMARY:"         <*> readStringTillEnd   <* newLine <|>
-           Tlocation    <$ token "LOCATION:"        <*> readStringTillEnd   <* newLine <|>
-           Tdescription <$ token "DESCRIPTION:"     <*> readStringTillEnd   <* newLine <|>
-           Tendevent    <$ token "END:VEVENT"                               <* newLine <|> 
-           Tendcal      <$ token "END:VCALENDAR"    <|> 
-           Tendcal      <$ token "END:VCALENDAR"                            <* newLine
-           where newLine = token "\r\n"
+anyToken = Tstartcal    <$ token printBeginCal                              <* newLine <|>
+           Tempty       <$ token printVersion                               <* newLine <|>
+           Tprodid      <$ token printProdID        <*> readStringTillEnd   <* newLine <|>
+           Tstartevent  <$ token printBeginEvent                            <* newLine <|> 
+           Tdtstamp     <$ token printDtStamp       <*> parseDateTime       <* newLine <|> 
+           Tdtstart     <$ token printDtStart       <*> parseDateTime       <* newLine <|> 
+           Tdtend       <$ token printDtEnd         <*> parseDateTime       <* newLine <|> 
+           Tuid         <$ token printUID           <*> readStringTillEnd   <* newLine <|>
+           Tsummary     <$ token printSummary       <*> readStringTillEnd   <* newLine <|>
+           Tlocation    <$ token printLocation      <*> readStringTillEnd   <* newLine <|>
+           Tdescription <$ token printDescription   <*> readStringTillEnd   <* newLine <|>
+           Tendevent    <$ token printEndEvent                              <* newLine <|> 
+           Tendcal      <$ token printEndCal                                           <|> 
+           Tendcal      <$ token printEndCal                                <* newLine
+           where newLine = token printCrlf
 
 scanCalendar :: Parser Char [Token]
 scanCalendar = greedy anyToken <* eof
@@ -136,71 +152,71 @@ fromStation (TStation x) = x
 fromStation = error "fromStation"
 
 -}
-
-pStartCalendar :: Parser Token Token
-pStartCalendar = (\x -> x) <$> satisfy checkType
-    where checkType Tstartcal = True
-          checkType _ = False
-
-pEndCalendar :: Parser Token Token
-pEndCalendar = (\x -> x) <$> satisfy checkType
-    where checkType Tendcal = True
-          checkType _ = False
-
-pStartVEvent :: Parser Token Token
-pStartVEvent = (\x -> x) <$> satisfy checkType
-    where checkType Tstartevent = True
-          checkType _ = False
-          
-pEndVEvent :: Parser Token Token
-pEndVEvent = (\x -> x) <$> satisfy checkType
-    where checkType Tendevent = True
-          checkType _ = False
-          
+--checkType :: (String -> Token) -> Token -> Bool ??
 
 pTprodid :: Parser Token String
 pTprodid = (\(Tprodid x) -> x) <$> satisfy checkType
     where checkType (Tprodid _) = True
           checkType _ = False
 
+pStartCalendar :: Parser Token Token
+pStartCalendar = satisfy checkType
+    where checkType Tstartcal = True
+          checkType _ = False
+
+pEndCalendar :: Parser Token Token
+pEndCalendar = satisfy checkType
+    where checkType Tendcal = True
+          checkType _ = False
+
+pStartVEvent :: Parser Token Token
+pStartVEvent = satisfy checkType
+    where checkType Tstartevent = True
+          checkType _ = False
+          
+pEndVEvent :: Parser Token Token
+pEndVEvent = satisfy checkType
+    where checkType Tendevent = True
+          checkType _ = False
 
 pTdtstamp :: Parser Token Token
-pTdtstamp = (\x -> x) <$> satisfy checkType
+pTdtstamp = satisfy checkType
     where checkType (Tdtstamp _) = True
           checkType _ = False
 
 pTdtstart :: Parser Token Token
-pTdtstart = (\x -> x) <$> satisfy checkType
+pTdtstart = satisfy checkType
     where checkType (Tdtstart _) = True
           checkType _ = False
 
 pTdtend :: Parser Token Token
-pTdtend = (\x -> x) <$> satisfy checkType
+pTdtend = satisfy checkType
     where checkType (Tdtend _) = True
           checkType _ = False
 
 pTuid :: Parser Token Token
-pTuid = (\x -> x) <$> satisfy checkType
+pTuid = satisfy checkType
     where checkType (Tuid _) = True
           checkType _ = False
 
+
 pTdescription :: Parser Token Token
-pTdescription = (\x -> x) <$> satisfy checkType
+pTdescription = satisfy checkType
     where checkType (Tdescription _) = True
           checkType _ = False
 
 pTlocation :: Parser Token Token
-pTlocation = (\x -> x) <$> satisfy checkType
+pTlocation = satisfy checkType
     where checkType (Tlocation _) = True
           checkType _ = False
 
 pTsummary :: Parser Token Token
-pTsummary = (\x -> x) <$> satisfy checkType
+pTsummary = satisfy checkType
     where checkType (Tsummary _) = True
           checkType _ = False
 
 pTempty :: Parser Token Token
-pTempty = (\x -> x) <$> satisfy checkType
+pTempty = satisfy checkType
     where checkType (Tempty) = True
           checkType _ = False
 {-
@@ -218,43 +234,64 @@ pVEvent = VEvent <$
 
 
 
-aa :: Parser Token Token
-aa = choice [   pTdtstamp,
-                pTuid,
-                pTdtstart, 
-                pTdtend,
-                pTdescription,
-                pTsummary,
-                pTlocation
+pVEvents :: Parser Token [VEvent]
+pVEvents = catMaybes <$> many pVEvent
+
+makeEventChoice :: Parser Token Token
+makeEventChoice = choice [   
+                    pTdtstamp,
+                    pTuid,
+                    pTdtstart, 
+                    pTdtend,
+                    pTdescription,
+                    pTsummary,
+                    pTlocation
                 ]
 
 pVEvent :: Parser Token (Maybe VEvent)
-pVEvent = f <$> many aa
-            where f props = do      
-                    dtstamp  <- exactlyOnce[p | (Tdtstamp p) <- props]
-                    uid      <-exactlyOnce [p | (Tuid p) <- props]
-                    dtstart  <- exactlyOnce[p | (Tdtstart p) <- props]
-                    dtend    <-exactlyOnce [p | (Tdtend p) <- props]
-                    desc     <-zeroOrOnce [p | (Tdescription p) <- props]
-                    summary  <- zeroOrOnce[p | (Tsummary p) <- props]
-                    location <- zeroOrOnce [p | (Tlocation p) <- props]
-                    return (VEvent dtstamp uid dtstart dtend desc summary location)
-exactlyOnce :: [a] -> Maybe a
-exactlyOnce []     = Nothing
-exactlyOnce [x]    = Just x
-exactlyOnce (x:xs) = Nothing
+pVEvent = pvevent <$ pStartVEvent <*> many makeEventChoice <* pEndVEvent
+            where
+                useProp [x] = Just x
+                useProp [] = Nothing
+                pvevent prop = do      
+                    dtstamp     <- useProp [x | (Tdtstamp x) <- prop]
+                    uid         <- useProp [x | (Tuid x) <- prop]
+                    dtstart     <- useProp [x | (Tdtstart x) <- prop]
+                    dtend       <- useProp [x | (Tdtend x) <- prop]
+                    description <- Just $ useProp [x | (Tdescription x) <- prop]
+                    summary     <- Just $ useProp [x | (Tsummary x) <- prop]
+                    location    <- Just $ useProp [x | (Tlocation x) <- prop]
+                    return (VEvent dtstamp uid dtstart dtend description summary location)
 
-zeroOrOnce :: [a] -> Maybe (Maybe a)
-zeroOrOnce []     = Just Nothing
-zeroOrOnce [x]    = Just (Just x)
-zeroOrOnce (x:xs) = Nothing
-
+{-
+pVEvent :: Parser Token VEvent
+pVEvent = VEvent <$> dtstamp <*> uid <*> dtstart <*> dtend <*> description <*> summary <*> location <* pStartVEvent <*> many makeEventChoice <* pEndVEvent
+            where
+                useProp [x] = Just x
+                useProp [] = Nothing
+                    
+                dtstamp prop = head [x | (Tdtstamp x) <- prop]
+                uid prop = head [x | (Tuid x) <- prop]
+                dtstart prop = head [x | (Tdtstart x) <- prop]
+                dtend prop = useProp [x | (Tdtend x) <- prop]
+                description prop = Just [x | (Tdescription x) <- prop]
+                summary prop = Just [x | (Tsummary x) <- prop]
+                location prop = Just [x | (Tlocation x) <- prop]
+-}
+pc :: Parser Token String
+pc = pTprodid <* pTempty <|> pTempty *> pTprodid
+{-
+pCalendar :: Parser Token (Maybe String)
+pCalendar = pcalendar <$> (many (choice[pTprodid,pTempty]))       
+        where
+            useProp x = Just $ head x 
+            pcalendar props = do
+                    prodid  <- useProp [p | (Tprodid p) <- props]
+                    empty <-   useProp [p | p@(Tempty) <- props]
+                    return (prodid)
+-}
 parseCalendar :: Parser Token Calendar
-parseCalendar = a <|> b
-    where a = Calendar <$ pStartCalendar <*> pTprodid <* pTempty <*> (catMaybes <$> many (pStartVEvent *> pVEvent <* pEndVEvent)) <* pEndCalendar
-          b= Calendar <$ pStartCalendar <* pTempty <*> pTprodid <*> (catMaybes <$> many (pStartVEvent *> pVEvent <* pEndVEvent)) <* pEndCalendar
-test = printCalendar $ fromJust $ Data.Text.Unsafe.inlinePerformIO $ readCalendar "examples/custom.ics"
-test2 =  printCalendar $ fromJust $ Data.Text.Unsafe.inlinePerformIO $ readCalendar "examples/rooster_infotc.ics"
+parseCalendar = Calendar <$ pStartCalendar <*> pc <*> pVEvents <* pEndCalendar  
 
 -- Exercise 2
 readCalendar :: FilePath -> IO (Maybe Calendar)
@@ -263,41 +300,30 @@ readCalendar fp = do
                     hSetNewlineMode h noNewlineTranslation
                     str <- hGetContents h
                     return (recognizeCalendar str)
-readCalendar2 :: FilePath -> IO (String)
-readCalendar2 fp = do 
-                    h <- openFile fp ReadMode
-                    hSetNewlineMode h noNewlineTranslation
-                    str <- hGetContents h
-                    return (str)
 
-dt = (DateTime (Date (Year 1997) (Month 07) (Day 14)) (Time (Hour 17) (Minute 01) (Second 01)) True) 
-dt' = (DateTime (Date (Year 1997) (Month 07) (Day 17)) (Time (Hour 17) (Minute 01) (Second 02)) True) 
-dt2 = (DateTime (Date (Year 1997) (Month 07) (Day 15)) (Time (Hour 17) (Minute 01) (Second 02)) True) 
-
-vev = (VEvent dt "19970610T172345Z-AF23B2@example.com" dt dt' (Just "a") (Just "a") (Just "a" ))
-exCal = Calendar "-//hacksw/handcal//NONSGML v1.0//EN" [vev]
 -- Exercise 3
 -- DO NOT use a derived Show instance. Your printing style needs to be nicer than that :)
 printCalendar :: Calendar -> String
-printCalendar (Calendar pid xs) = printBeginCal++ "\r\n" ++ "PRODID:" ++ pid ++ "\r\n"++ concatMap printVEvent xs ++  printEndCal 
+printCalendar (Calendar pid xs) =   printBeginCal       ++ printCrlf ++ 
+                                    printVersion        ++ printCrlf ++ 
+                                    printProdID ++ pid  ++ printCrlf ++ 
+                                    concatMap printVEvent xs       ++  
+                                    printEndCal 
 
 printVEvent :: VEvent -> String
-printVEvent (VEvent stamp uid s e d summ loc) = printBeginEvent ++ "\r\n" ++
-                                                "DTSTAMP:" ++ printDateTime stamp ++ "\r\n"++ 
-                                                "DTSTART:" ++ printDateTime s++ "\r\n" ++ 
-                                                "DTEND:" ++ printDateTime e ++ "\r\n" ++ 
-                                                "UID:" ++ uid ++ "\r\n"++ 
-                                                printMaybe d "DESCRIPTION:" ++ 
-                                                printMaybe summ "SUMMARY:" ++ 
-                                                printMaybe loc "LOCATION:" ++
-                                                printEndEvent ++ "\r\n"
+printVEvent (VEvent stamp uid s e d summ loc) = printBeginEvent                     ++ printCrlf ++
+                                                printDtStamp ++ printDateTime stamp ++ printCrlf ++ 
+                                                printDtStart ++ printDateTime s     ++ printCrlf ++ 
+                                                printDtEnd   ++ printDateTime e     ++ printCrlf ++ 
+                                                printUID     ++ uid                 ++ printCrlf ++ 
+                                                printMaybe d    printDescription ++ 
+                                                printMaybe summ printSummary ++ 
+                                                printMaybe loc  printLocation ++
+                                                printEndEvent ++ printCrlf
 
-printBeginCal = "BEGIN:VCALENDAR"
-printEndCal = "END:VCALENDAR"
-printBeginEvent = "BEGIN:VEVENT"
-printEndEvent = "END:VEVENT"
 
-printMaybe (Just x) p = p ++ x ++ "\r\n"
+printMaybe :: (Maybe String) -> String -> String
+printMaybe (Just x) p = p ++ x ++ printCrlf
 printMaybe Nothing _ = ""
 
 printDateTime :: DateTime -> String
@@ -315,41 +341,63 @@ showData a = if null xs then "0" ++ [x] else o
     where o@(x:xs) = show a
 
 -- Exercise 4
+--count events
 countEvents :: Calendar -> Int
 countEvents = length . events
 
+--find events
 findEvents :: DateTime -> Calendar -> [VEvent]
-findEvents dt (Calendar _ xs) = foldr (\c ts -> (checkEvent dt c xs) ++ ts) [] xs
+findEvents dt (Calendar _ xs) = filter (checkEvent dt) xs
 
-checkEvent :: DateTime -> VEvent -> [VEvent] -> [VEvent]
-checkEvent dt e xs = if start < dt && end > dt then e:xs else xs
+checkEvent :: DateTime -> VEvent -> Bool
+checkEvent dt e = start <= dt && end > dt 
     where start = dtStart e
           end = dtEnd e
 
-
+--checkoverlap
 checkOverlapping :: Calendar -> Bool
-checkOverlapping c = any (\cur-> if findEvents (dtStart cur) c /= [] && findEvents (dtEnd cur) c /= [] then True else False) (events c)
+checkOverlapping c = any (\cur-> findEvents (dtStart cur) c /= [] && findEvents (dtEnd cur) c /= []) (events c)
 
-
+--time spent
 timeSpent :: String -> Calendar -> Int
-timeSpent s (Calendar _ xs) = undefined
+timeSpent s c = sum (map eventTime (findSummary s c))
 
-daysDif :: Date -> Date -> Int
-daysDif x y = yearDif + (dayBegin - countDays y)
-    where yearDif = sum (map daysInYear [(unYear (year x)) .. (unYear (year y))])
-          dayBegin = daysInYear (unYear (year x)) - countDays x
-          countDays (Date _ (Month 1) d) = unDay d
-          countDays d = sum (map (flip daysInMonth' (year d)) [1..((unMonth (month d)) - 1)]) + unDay (day d)
+maybeToString :: (Maybe String) -> String
+maybeToString (Just x) = x
+maybeToString Nothing = ""
+
+findSummary :: String -> Calendar -> [VEvent]
+findSummary s (Calendar _ xs) = foldr (\c rs -> if (maybeToString (summary c)) == s then c:rs else rs) [] xs
+
+eventTime :: VEvent -> Int
+eventTime e = (((daysTillYearEnds bd) + sum (map daysInYear years) - (daysTillYearEnds ed)) * 24 + h) * 60 + m + (div s 60)
+    where bd = date $ dtStart e
+          ed = date $ dtEnd e
+          bt = time $ dtStart e
+          et = time $ dtEnd e
+          by = unYear (year bd)
+          ey = unYear (year ed)
+          h = unHour (hour bt) - unHour (hour et)
+          m = unMinute (minute bt) - unMinute (minute et)
+          s = unSecond (second bt) - unSecond (second et)
+          years = if by == ey then [] else [by .. ey - 1]
+
+daysTillYearEnds :: Date -> Int
+daysTillYearEnds (Date y m (Day d)) = sum (map (flip daysInMonth' y) (xs (unMonth m))) - d
+    where xs 12 = [12]
+          xs x = [x..12]
+
+
 data EventWeek = EventWeek { eventdays :: [EventDay]}
 
-data EventDay = EventDay {  unDate :: Date }
-                            --unEvents :: [VEvent] }
+data EventDay = EventDay {  unDate :: Date,
+                            unEvents :: [VEvent] }
 
 -- Exercise 5
 ppMonth :: Year -> Month -> Calendar -> Doc
-ppMonth y m c = text "" --text (concatMap printEventWeek (createCalendar c (Date y m (Day 1))))
+ppMonth y m c = undefined--text (concatMap printEventWeek (createCalendar c (Date y m (Day 1))))
 {-
-printEventWeek = concatMap printEventDay [(EventDay (Date (Year 1) (Month 12) (Day 1))), 
+printEventWeek = concatMap ppEventDay [(EventDay (Date (Year 1) (Month 12) (Day 1))), 
                                             (EventDay (Date (Year 1) (Month 12) (Day 2))),
                                     (EventDay (Date (Year 1) (Month 12) (Day 3))),
                                     (EventDay (Date (Year 1) (Month 12) (Day 4))),
@@ -357,15 +405,22 @@ printEventWeek = concatMap printEventDay [(EventDay (Date (Year 1) (Month 12) (D
                                     (EventDay (Date (Year 1) (Month 12) (Day 6))),
                                     (EventDay (Date (Year 1) (Month 12) (Day 7)))    
                                     ]
-ppVEvent e = undefined-dtStart e dtEnd e
+-}
+
+ppVEvent :: VEvent -> String
+ppVEvent e = ppTime start ++ " - " ++ ppTime end
+    where ppTime dt = showData (unHour (hour dt)) ++ ":" ++ showData (unMinute (minute dt))
+          start = time $ dtStart e
+          end = time $ dtEnd e
+   {-
 ppEventDay (EventDay d) = if mod cday 7 == 1 then printEDay else "| " ++ printEDay
     where cday = (unDay (day d))
           printEDay = show cday ++ replicate 13 ' '
-
+-}
 ppDottedLine :: String
 ppDottedLine = line 14 ++ concat (replicate 5 ("+" ++ line 15)) ++ line 15
-    where line n = (replicate n '-')-}
-{-
+    where line n = (replicate n '-')
+
 createCalendar :: Calendar -> Date -> [EventWeek]
 createCalendar c d = map (createEventWeek c) xs
     where xs = chunksOf 7 (createDateList d [1..(daysInMonth (month d) (year d))])
@@ -376,10 +431,12 @@ createEventWeek c xs = EventWeek $ map (findEventsDay c) xs
 findEventsDay :: Calendar -> Date -> EventDay
 findEventsDay (Calendar _ xs) d = EventDay (d) (foldr (\e as -> if date (dtStart e) == d then e : as else as) [] xs)
 
+eventWeekHeight :: EventWeek -> Int
+eventWeekHeight e = foldr (\c x -> x + length (unEvents c)) 0 (eventdays e)
 
 createDateList :: Date -> [Int] -> [Date]
 createDateList (Date (Year y) (Month m) (Day d)) ds = foldr (\c xs -> ((Date (Year y) (Month m) (Day c)):xs)) [] ds
--}
+
 --returns days in month (needs year for leap year check)   
 daysInMonth' :: Int -> Year -> Int
 daysInMonth' m y = daysInMonth (Month m) y                         
